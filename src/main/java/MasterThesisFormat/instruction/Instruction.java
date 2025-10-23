3d25b0fe0519b097ee9092cd541e46e312bfb9c9
Shabi99 <83295680+Shabi-Dev@users.noreply.github.com>
2025-10-23 19:57:34 +0200

    Refactor loads instructions

--- src/main/java/MasterThesisFormat/instruction/Instruction.java
+++ src/main/java/MasterThesisFormat/instruction/Instruction.java
@@ -192,43 +192,29 @@ public class Instruction {
         }
     }
 
-    public static byte[] load(byte value, byte register) throws IOException {
-        return load1(new byte[]{value}, register);
-    }
-
-
-    public static byte[] load1(byte value, byte register) throws IOException {
-        return load1(new byte[]{value}, register);
-    }
-
     public static byte[] load1(byte[] value, byte register) throws IOException {
-        if (value.length > 0xFF) {
-            throw new IOException("Value too large for LOAD1");
-        }
-        return encodeLoad(OpCode.LOAD1, value.length, value, register, 1);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(OpCode.LOAD1.getCode());
+        out.write(value[0]);
+        out.write(register);
+        return out.toByteArray();
     }
 
     public static byte[] load2(byte[] value, byte register) throws IOException {
-        if (value.length > 0xFFFF) {
-            throw new IOException("Value too large for LOAD2");
-        }
-        return encodeLoad(OpCode.LOAD2, value.length, value, register, 2);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(OpCode.LOAD2.getCode());
+        out.write(value[0]);
+        out.write(value[1]);
+        out.write(register);
+        return out.toByteArray();
     }
 
     public static byte[] load3(byte[] value, byte register) throws IOException {
-        if (value.length > 0xFFFFFF) {
-            throw new IOException("Value too large for LOAD3");
-        }
-        return encodeLoad(OpCode.LOAD3, value.length, value, register, 3);
-    }
-
-    private static byte[] encodeLoad(OpCode opCode, int length, byte[] value, byte register, int lengthBytes) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream(1 + lengthBytes + length + 1);
-        out.write(opCode.getCode());
-        for (int shift = (lengthBytes - 1) * 8; shift >= 0; shift -= 8) {
-            out.write((length >> shift) & 0xFF);
-        }
-        out.write(value);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(OpCode.LOAD3.getCode());
+        out.write(value[0]);
+        out.write(value[1]);
+        out.write(value[2]);
         out.write(register);
         return out.toByteArray();
     }
